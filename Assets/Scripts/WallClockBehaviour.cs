858639e30ebd115899017df0d53456207f826516
Felipe Morais <felipeprodev@hotmail.com>
2020-10-03 12:14:43 -0300

    Some Changes

---
+++ Assets/Scripts/WallClockBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class WallClockBehaviour : MonoBehaviour
+    {
+        [SerializeField]
+        private float _rotationSpeed = 1.5f;
+
+        public float RotationSpeed
+        {
+            get => _rotationSpeed;
+            set => _rotationSpeed = value;
+        }
+
+        [SerializeField]
+        private Transform _secondPointer;
+        public Transform SecondPointer
+        {
+            get => _secondPointer;
+            set => _secondPointer = value;
+        }
+
+        private void Update()
+        {
+            _secondPointer.Rotate(0f, 0f, _secondPointer.rotation.z + _rotationSpeed + Time.deltaTime, Space.Self);
+        }
+
+    }
+}
